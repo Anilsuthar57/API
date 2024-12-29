d4ef96b8baae23c39de30657b49b84822e30322e
Anil Suthar <anilsuthar0057@gmail.com>
2024-12-29 19:36:30 +0530

    learn promises

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
-const img = document.querySelector('img')
-const button = document.querySelector('button')
+// const img = document.querySelector('img')
+// const button = document.querySelector('button')
 
 
 
-button.addEventListener('click', () => {
+// button.addEventListener('click', () => {
 
-    // fetch('https://dog.ceo/api/breeds/image/random')
-    // .then(response => response.json())
-    // .then(json => {
-    //     img.src = json.message
-    // })
+//     // fetch('https://dog.ceo/api/breeds/image/random')
+//     // .then(response => response.json())
+//     // .then(json => {
+//     //     img.src = json.message
+//     // })
 
+//     const xhr = new XMLHttpRequest()
+
+//     console.log(xhr)
+//     xhr.responseType = "json"
+//     // xhr.addEventListener('load', () => {
+//     //     console.log(xhr.response)
+//     //     img.src = xhr.response.message
+//     // })
+
+//     xhr.onload = () => {
+//             console.log(xhr.response)
+//             img.src = xhr.response.message
+//         }
+
+
+//     xhr.open('Get', 'https://dog.ceo/api/breeds/image/random')
+//     xhr.send()
+
+
+// })
+
+function makeHttpRequest(method, url) {
     const xhr = new XMLHttpRequest()
-
-    console.log(xhr)
-    xhr.responseType = "json"
-    // xhr.addEventListener('load', () => {
-    //     console.log(xhr.response)
-    //     img.src = xhr.response.message
-    // })
-
-    xhr.onload = () => {
-            console.log(xhr.response)
-            img.src = xhr.response.message
-        }
+    xhr.responseType = 'json'
 
 
-    xhr.open('Get', 'https://dog.ceo/api/breeds/image/random')
+    const promises = new promises()
+     
+
+    
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response)
+    })
+    xhr.open(method, url)
     xhr.send()
+}
 
 
-})
+// makeHttpRequest('GET', 'https://dummyjson.com/users/1',(userdata)=>{
+//     makeHttpRequest('GET','https://dummyjson.com/users/2',(userdata=>{
+//         makeHttpRequest('GET','https://dummyjson.com/users/3',(userdata)=>{
+//             console.log(userdata)
+//         })
+//     }))
+// })
+
 
